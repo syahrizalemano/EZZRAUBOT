c027fdfed66034c6a661dad385a71e1e0b89def4
syahrizalemano <89107294+syahrizalemano@users.noreply.github.com>
2021-12-25 17:31:56 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -94,7 +94,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN") or None
 
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
-                     or "https://github.com/alfianandaa/ProjectAlf")
+                     or "https://github.com/syahrizalemano/EZZRAUBOT")
 
 # UPSTREAM_REPO_URL branch, the default is master
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "master"
